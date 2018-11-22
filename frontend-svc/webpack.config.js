a12c468d277f71ed318bbd5f108e6be05f921eeb
cookeem <cookeem@qq.com>
2018-11-22 15:47:58 +0800

    update frontend

--- frontend-svc/webpack.config.js
+++ frontend-svc/webpack.config.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 module.exports = {
 	entry: './like_button.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
 	module: {
 		rules: [
 			{
