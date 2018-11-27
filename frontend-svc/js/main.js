9697fc5604221c7794c93fdf00802fc1a0055ef0
cookeem <cookeem@qq.com>
2018-11-27 16:21:37 +0800

    update main.js

--- frontend-svc/js/main.js
+++ frontend-svc/js/main.js
@@ -13,3 +13,23 @@ const Index = () => {
 };
 
 ReactDOM.render(<Index />, document.getElementById('index'));
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Zeng',
+  lastName: 'Haijian'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('hello')
+);
