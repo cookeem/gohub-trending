b1b7ac91c29e680c45273c3990418ab12eafb2b4
cookeem <cookeem@qq.com>
2019-01-29 10:18:27 +0800

    update config.js for production

--- frontend-svc/app/config.js
+++ frontend-svc/app/config.js
@@ -1,3 +1,6 @@
-// export const backendUri = '';
-//如果要使用npm run start，请替换为以下内容
-export const backendUri = 'http://localhost:3000';
+// 如果要发布到正式环境，请替换为以下内容
+export const backendUri = '';
+
+
+// 如果要使用npm run start，作为开发调试，请替换为以下内容
+// export const backendUri = 'http://localhost:3000';
