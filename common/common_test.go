cc5e3e7888351f807b8b7411d602655427ee7c3c
cookeem <cookeem@qq.com>
2019-02-21 15:23:14 +0800

    add common test case

---
+++ common/common_test.go
@@ -0,0 +1,51 @@
+package common
+
+import (
+	"testing"
+)
+
+type testObj struct {
+	username string
+	uid      int
+}
+
+func TestCreateTokenString(t *testing.T) {
+	secretStr := "iZXhwIjoxNTQyODY2ODA0LCJpYXQiOjE1NDI4NjU5MDQsImlzcyI6Imdp"
+	expSecs := 15 * 60
+	// 测试用例
+	testCases := []struct {
+		name   string
+		given  testObj
+		expect testObj
+	}{
+		{
+			name:   "english-username",
+			given:  testObj{username: "cookeem", uid: 1},
+			expect: testObj{username: "cookeem", uid: 1},
+		},
+		{
+			name:   "chinese-username",
+			given:  testObj{username: "小明", uid: 2},
+			expect: testObj{username: "小明", uid: 2},
+		},
+		{
+			name:   "negative-uid",
+			given:  testObj{username: "haijian", uid: -1},
+			expect: testObj{username: "haijian", uid: -1},
+		},
+	}
+	for _, tc := range testCases {
+		// 测试CreateTokenString是否正常输出token
+		token, err := CreateTokenString(tc.given.username, tc.given.uid, secretStr, expSecs)
+		if err != nil {
+			t.Errorf("testcase %q: expected %v, got %v", tc.name, tc.expect, err)
+		} else {
+			t.Logf("testcase %q: create token: %v", tc.name, token)
+			// 验证CreateTokenString输出的token是否能够正常校验
+			ut := VerifyTokenString(token, secretStr)
+			if ut.Username != tc.expect.username || ut.Uid != tc.expect.uid {
+				t.Errorf("testcase %q: expected %v, got %v", tc.name, tc.expect, testObj{username: ut.Username, uid: ut.Uid})
+			}
+		}
+	}
+}
