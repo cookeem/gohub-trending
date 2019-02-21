89f076b2826bbb86b9cff4610e80ee9d4c5349cb
cookeem <cookeem@qq.com>
2019-02-21 15:47:20 +0800

    update test case

--- common/common_test.go
+++ common/common_test.go
@@ -12,7 +12,7 @@ type testObj struct {
 func TestCreateTokenString(t *testing.T) {
 	secretStr := "iZXhwIjoxNTQyODY2ODA0LCJpYXQiOjE1NDI4NjU5MDQsImlzcyI6Imdp"
 	expSecs := 15 * 60
-	// 测试用例
+	// 测试用例清单
 	testCases := []struct {
 		name   string
 		given  testObj
@@ -34,14 +34,15 @@ func TestCreateTokenString(t *testing.T) {
 			expect: testObj{username: "haijian", uid: -1},
 		},
 	}
+	//对测试用例进行遍历
 	for _, tc := range testCases {
-		// 测试CreateTokenString是否正常输出token
+		// 测试CreateTokenString是否正常输出jwt
 		token, err := CreateTokenString(tc.given.username, tc.given.uid, secretStr, expSecs)
 		if err != nil {
 			t.Errorf("testcase %q: expected %v, got %v", tc.name, tc.expect, err)
 		} else {
 			t.Logf("testcase %q: create token: %v", tc.name, token)
-			// 验证CreateTokenString输出的token是否能够正常校验
+			// 验证CreateTokenString输出的jwt是否能够通过VerifyTokenString函数正常校验
 			ut := VerifyTokenString(token, secretStr)
 			if ut.Username != tc.expect.username || ut.Uid != tc.expect.uid {
 				t.Errorf("testcase %q: expected %v, got %v", tc.name, tc.expect, testObj{username: ut.Username, uid: ut.Uid})
